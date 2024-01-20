5c0bcc1d648bbfb2ce0fe52407dbb29e417bb484
esara <esara@causely.io>
2024-01-19 21:18:34 -0500

    kraft - zookeeper-less apache kafka cluster

--- qsh.sh
+++ qsh.sh
@@ -49,7 +49,7 @@ helm upgrade --install prometheus-postgres-exporter prometheus-community/prometh
 
 # https://github.com/strimzi/strimzi-kafka-operator/tree/main/helm-charts/helm3/strimzi-kafka-operator
 helm repo add strimzi https://strimzi.io/charts/
-helm upgrade --install strimzi-kafka-operator strimzi/strimzi-kafka-operator -n ${NS}
+helm upgrade --install strimzi-kafka-operator strimzi/strimzi-kafka-operator -n ${NS} --set featureGates=+UseKRaft
 helm upgrade --install kafka-exporter prometheus-community/prometheus-kafka-exporter --namespace=quarkus-super-heroes --values super-heroes/kubernetes/exporters/prometheus_kafka_values.yaml
 
 kubectl apply -n ${NS} -f super-heroes/kubernetes
