ee8abefd6bb95e5258125c4a948fcf7cdfcdfd9d
Endre Sara <endresara@gmail.com>
2026-01-09 22:30:40 -0500

    remove otel instrumentation

--- grpc-locations/src/main/kotlin/io/quarkus/workshop/superheroes/location/service/LocationService.kt
+++ grpc-locations/src/main/kotlin/io/quarkus/workshop/superheroes/location/service/LocationService.kt
@@ -1,7 +1,5 @@
 package io.quarkus.workshop.superheroes.location.service
 
-import io.opentelemetry.instrumentation.annotations.SpanAttribute
-import io.opentelemetry.instrumentation.annotations.WithSpan
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.transaction.Transactional
 import io.quarkus.workshop.superheroes.location.Location
@@ -9,25 +7,20 @@ import io.quarkus.workshop.superheroes.location.repository.LocationRepository
 
 @ApplicationScoped
 class LocationService(private val locationRepository: LocationRepository) {
-	@WithSpan("LocationService.getRandomLocation")
   fun getRandomLocation() = this.locationRepository.findRandom()
 
-	@WithSpan("LocationService.getAllLocations")
 	fun getAllLocations() = this.locationRepository.listAll()
 
-	@WithSpan("LocationService.getLocationByName")
-	fun getLocationByName(@SpanAttribute("arg.name") name: String?) = when(name) {
+	fun getLocationByName(name: String?) = when(name) {
 		null -> null
 		else -> this.locationRepository.findByName(name)
 	}
 
-	@WithSpan("LocationService.deleteAllLocations")
 	@Transactional
 	fun deleteAllLocations() {
 		this.locationRepository.deleteAll()
 	}
 
-	@WithSpan("LocationService.replaceAllLocations")
 	@Transactional
 	fun replaceAllLocations(locations: Collection<Location>) {
 		this.locationRepository.deleteAll()
